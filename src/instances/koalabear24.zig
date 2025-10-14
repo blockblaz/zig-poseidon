a57a3dd4dd383065a31287a1e3d35204a0658df4
ch4r10t33r <psramanuj@gmail.com>
2025-10-14 22:14:50 +0100

    fix: bug fixes in koalabear24 instance

--- src/instances/koalabear24.zig
+++ src/instances/koalabear24.zig
@@ -11,30 +11,30 @@ const SBOX_DEGREE = 3; // KoalaBear uses S-Box degree 3
 // V = [-2, 1, 2, 1/2, 3, 4, -1/2, -3, -4, 1/2^8, 1/4, 1/8, 1/16, 1/32, 1/64, 1/2^24,
 //      -1/2^8, -1/8, -1/16, -1/32, -1/64, -1/2^7, -1/2^9, -1/2^24]
 const DIAGONAL = [WIDTH]u32{
-    parseHex("7efffffe"), // -2
+    parseHex("7effffff"), // -2
     parseHex("00000001"), // 1
     parseHex("00000002"), // 2
     parseHex("3f800001"), // 1/2
     parseHex("00000003"), // 3
     parseHex("00000004"), // 4
     parseHex("3f800000"), // -1/2
-    parseHex("7ffffffd"), // -3
-    parseHex("7ffffffc"), // -4
-    parseHex("007f0000"), // 1/2^8
-    parseHex("1fc00000"), // 1/4
-    parseHex("0fe00000"), // 1/8
-    parseHex("07f00000"), // 1/16
-    parseHex("03f80000"), // 1/32
-    parseHex("01fc0000"), // 1/64
-    parseHex("00000080"), // 1/2^24
-    parseHex("7f00ffff"), // -1/2^8
-    parseHex("70200001"), // -1/8
-    parseHex("78000001"), // -1/16
-    parseHex("7c000001"), // -1/32
-    parseHex("7e000001"), // -1/64
-    parseHex("7f010000"), // -1/2^7
-    parseHex("7f008000"), // -1/2^9
-    parseHex("7fffff7f"), // -1/2^24
+    parseHex("7efffffe"), // -3
+    parseHex("7efffffd"), // -4
+    parseHex("7e810001"), // 1/2^8
+    parseHex("5f400001"), // 1/4
+    parseHex("6f200001"), // 1/8
+    parseHex("77100001"), // 1/16
+    parseHex("7b080001"), // 1/32
+    parseHex("7d040001"), // 1/64
+    parseHex("7effff82"), // 1/2^24
+    parseHex("007f0000"), // -1/2^8
+    parseHex("0fe00000"), // -1/8
+    parseHex("07f00000"), // -1/16
+    parseHex("03f80000"), // -1/32
+    parseHex("01fc0000"), // -1/64
+    parseHex("00fe0000"), // -1/2^7
+    parseHex("003f8000"), // -1/2^9
+    parseHex("0000007f"), // -1/2^24
 };
 
 pub const Poseidon2KoalaBear = poseidon2.Poseidon2(
@@ -48,82 +48,84 @@ pub const Poseidon2KoalaBear = poseidon2.Poseidon2(
     INTERNAL_RCS,
 );
 
-// External round constants from plonky3 KoalaBear width-24 (8 rounds: 4 initial + 4 final)
+// External round constants from plonky3 KoalaBear width-24
+// 8 rounds total: 4 initial (beginning) + 4 final (end)
+// Source: https://github.com/Plonky3/Plonky3/blob/main/koala-bear/src/poseidon2.rs
 const EXTERNAL_RCS = [EXTERNAL_ROUNDS][WIDTH]u32{
     .{ // Round 0 (initial)
-        parseHex("1d050e2c"), parseHex("6cf27aed"), parseHex("6280e94d"), parseHex("267f7d1d"),
-        parseHex("3e38f61e"), parseHex("032d3068"), parseHex("5a90f75c"), parseHex("015a0f76"),
-        parseHex("6967d6dc"), parseHex("0dbbea00"), parseHex("5889b859"), parseHex("2a05f0ee"),
-        parseHex("553a8e55"), parseHex("651ea135"), parseHex("5477b8cf"), parseHex("42713618"),
-        parseHex("3e7a4c3c"), parseHex("345aea59"), parseHex("2c03c6b9"), parseHex("0ed91594"),
-        parseHex("5f7f5289"), parseHex("017726de"), parseHex("0ea1e531"), parseHex("15ba6952"),
+        parseHex("1d0939dc"), parseHex("6d050f8d"), parseHex("628058ad"), parseHex("2681385d"),
+        parseHex("3e3c62be"), parseHex("032cfad8"), parseHex("5a91ba3c"), parseHex("015a56e6"),
+        parseHex("696b889c"), parseHex("0dbcd780"), parseHex("5881b5c9"), parseHex("2a076f2e"),
+        parseHex("55393055"), parseHex("6513a085"), parseHex("547ac78f"), parseHex("4281c5b8"),
+        parseHex("3e7a3f6c"), parseHex("34562c19"), parseHex("2c04e679"), parseHex("0ed78234"),
+        parseHex("5f7a1aa9"), parseHex("0177640e"), parseHex("0ea4f8d1"), parseHex("15be7692"),
     },
     .{ // Round 1 (initial)
-        parseHex("6f81ba22"), parseHex("6e876b46"), parseHex("71eee23a"), parseHex("31d5c653"),
-        parseHex("21267e2f"), parseHex("150446ab"), parseHex("1eb8e60e"), parseHex("3a05fa0b"),
-        parseHex("38df4871"), parseHex("440b2f96"), parseHex("6b02c356"), parseHex("243d8052"),
-        parseHex("6ddf3198"), parseHex("78dfceaa"), parseHex("4be36ceb"), parseHex("3d4df117"),
-        parseHex("75e90790"), parseHex("395cf215"), parseHex("26db3e24"), parseHex("7da12029"),
-        parseHex("0cb9cebf"), parseHex("0567a770"), parseHex("5bdb20f6"), parseHex("1356ddce"),
+        parseHex("6eafdd62"), parseHex("71a572c6"), parseHex("72416f0a"), parseHex("31ce1ad3"),
+        parseHex("2136a0cf"), parseHex("1507c0eb"), parseHex("1eb6e07a"), parseHex("3a0ccf7b"),
+        parseHex("38e4bf31"), parseHex("44128286"), parseHex("6b05e976"), parseHex("244a9b92"),
+        parseHex("6e4b32a8"), parseHex("78ee2496"), parseHex("4761115b"), parseHex("3d3a7077"),
+        parseHex("75d3c670"), parseHex("396a2475"), parseHex("26dd00b4"), parseHex("7df50f59"),
+        parseHex("0cb922df"), parseHex("0568b190"), parseHex("5bd3fcd6"), parseHex("1351f58e"),
     },
     .{ // Round 2 (initial)
-        parseHex("523e3332"), parseHex("4c08a97f"), parseHex("7ec2b4a6"), parseHex("29b9f6bd"),
-        parseHex("5a8d9b8f"), parseHex("6af9e67e"), parseHex("08a1cab2"), parseHex("4e6c72e6"),
-        parseHex("6f7aa8bd"), parseHex("584ea34b"), parseHex("42db0d50"), parseHex("36a3c62a"),
-        parseHex("6f4e95fb"), parseHex("19e79a1f"), parseHex("11f08016"), parseHex("3d80c7b4"),
-        parseHex("52736b1f"), parseHex("51db7fb6"), parseHex("04a85c13"), parseHex("38d618f6"),
-        parseHex("3ea9cfa0"), parseHex("4b1bad2b"), parseHex("65d90f9f"), parseHex("6816bd24"),
+        parseHex("52191b5f"), parseHex("119171b8"), parseHex("1e8bb727"), parseHex("27d21f26"),
+        parseHex("36146613"), parseHex("1ee817a2"), parseHex("71abe84e"), parseHex("44b88070"),
+        parseHex("5dc04410"), parseHex("2aeaa2f6"), parseHex("2b7bb311"), parseHex("6906884d"),
+        parseHex("0522e053"), parseHex("0c45a214"), parseHex("1b016998"), parseHex("479b1052"),
+        parseHex("3acc89be"), parseHex("0776021a"), parseHex("7a34a1f5"), parseHex("70f87911"),
+        parseHex("2caf9d9e"), parseHex("026aff1b"), parseHex("2c42468e"), parseHex("67726b45"),
     },
     .{ // Round 3 (initial)
-        parseHex("2343b4c7"), parseHex("325f4080"), parseHex("055f01fa"), parseHex("17bb3a53"),
-        parseHex("146a31c1"), parseHex("261e7e62"), parseHex("2e769679"), parseHex("16bc52bf"),
-        parseHex("104e47fe"), parseHex("26c50e91"), parseHex("306b64fc"), parseHex("01d6e875"),
-        parseHex("2ca33222"), parseHex("01cb0a48"), parseHex("2b6e6a59"), parseHex("2dab6e3f"),
-        parseHex("20e04a6b"), parseHex("2ead4396"), parseHex("2b013abc"), parseHex("1ace7fc3"),
-        parseHex("29722b61"), parseHex("16018c9f"), parseHex("1cd2e9d7"), parseHex("31e82356"),
+        parseHex("09b6f53c"), parseHex("73d76589"), parseHex("5793eeb0"), parseHex("29e720f3"),
+        parseHex("75fc8bdf"), parseHex("4c2fae0e"), parseHex("20b41db3"), parseHex("7e491510"),
+        parseHex("2cadef18"), parseHex("57fc24d6"), parseHex("4d1ade4a"), parseHex("36bf8e3c"),
+        parseHex("3511b63c"), parseHex("64d8476f"), parseHex("732ba706"), parseHex("46634978"),
+        parseHex("0521c17c"), parseHex("5ee69212"), parseHex("3559cba9"), parseHex("2b33df89"),
+        parseHex("653538d6"), parseHex("5fde8344"), parseHex("4091605d"), parseHex("2933bdde"),
     },
     .{ // Round 4 (final)
-        parseHex("139e28ca"), parseHex("5dcd4b89"), parseHex("52007c17"), parseHex("133c42e9"),
-        parseHex("39af83b3"), parseHex("45e4fcc5"), parseHex("75c3cf54"), parseHex("59a58639"),
-        parseHex("700f956b"), parseHex("02671e29"), parseHex("44c7f0dc"), parseHex("2215b19f"),
-        parseHex("51409c03"), parseHex("5921e3cf"), parseHex("67464618"), parseHex("397e1773"),
-        parseHex("2d6e7df8"), parseHex("70823ae7"), parseHex("3b40ea7d"), parseHex("2ad0663c"),
-        parseHex("0cb558a7"), parseHex("23f0f8b8"), parseHex("3df76ea9"), parseHex("7d29aec9"),
+        parseHex("1395d4ca"), parseHex("5dbac049"), parseHex("51fc2727"), parseHex("13407399"),
+        parseHex("39ac6953"), parseHex("45e8726c"), parseHex("75a7311c"), parseHex("599f82c9"),
+        parseHex("702cf13b"), parseHex("026b8955"), parseHex("44e09bbc"), parseHex("2211207f"),
+        parseHex("5128b4e3"), parseHex("591c41af"), parseHex("674f5c68"), parseHex("3981d0d3"),
+        parseHex("2d82f898"), parseHex("707cd267"), parseHex("3b4cca45"), parseHex("2ad0dc3c"),
+        parseHex("0cb79b37"), parseHex("23f2f4e8"), parseHex("3de4e739"), parseHex("7d232359"),
     },
     .{ // Round 5 (final)
-        parseHex("389b4187"), parseHex("257bc0b1"), parseHex("4596d6fb"), parseHex("0d4f6502"),
-        parseHex("5b03e9bf"), parseHex("3bf32bc9"), parseHex("7c4d1e67"), parseHex("6627f196"),
-        parseHex("2c02da3a"), parseHex("0ad3ab11"), parseHex("5fb1c9b2"), parseHex("090c6af2"),
-        parseHex("097e898b"), parseHex("07553c05"), parseHex("06a9fc45"), parseHex("43b65edf"),
-        parseHex("1e8db134"), parseHex("17f9adff"), parseHex("4e8d38b2"), parseHex("0d9876d8"),
-        parseHex("0b6b33b6"), parseHex("4e95997c"), parseHex("14d75737"), parseHex("2c56c8e3"),
+        parseHex("389d82f9"), parseHex("259b2e6c"), parseHex("45a94def"), parseHex("0d497380"),
+        parseHex("5b049135"), parseHex("3c268399"), parseHex("78feb2f9"), parseHex("300a3eec"),
+        parseHex("505165bb"), parseHex("20300973"), parseHex("2327c081"), parseHex("1a45a2f4"),
+        parseHex("5b32ea2e"), parseHex("2d5d1a70"), parseHex("053e613e"), parseHex("5433e39f"),
+        parseHex("495529f0"), parseHex("1eaa1aa9"), parseHex("578f572a"), parseHex("698ede71"),
+        parseHex("5a0f9dba"), parseHex("398a2e96"), parseHex("0c7b2925"), parseHex("2e6b9564"),
     },
     .{ // Round 6 (final)
-        parseHex("229e77aa"), parseHex("23cc39df"), parseHex("20368ae5"), parseHex("231df374"),
-        parseHex("162ce741"), parseHex("0435fe23"), parseHex("27aac3bb"), parseHex("16c3613e"),
-        parseHex("2786997a"), parseHex("00f81e2d"), parseHex("1a981f0b"), parseHex("2343e351"),
-        parseHex("29ce7fef"), parseHex("131f0240"), parseHex("22c94593"), parseHex("28b33e32"),
-        parseHex("0fad1add"), parseHex("60b4a4be"), parseHex("2a9ad1b9"), parseHex("2b3002d9"),
-        parseHex("65313676"), parseHex("5fdc26a4"), parseHex("408d1a5d"), parseHex("291e6e7e"),
+        parseHex("026b00de"), parseHex("7644c1e9"), parseHex("5c23d0bd"), parseHex("3470b5ef"),
+        parseHex("6013cf3a"), parseHex("48747288"), parseHex("13b7a543"), parseHex("3eaebd44"),
+        parseHex("0004e60c"), parseHex("1e8363a2"), parseHex("2343259a"), parseHex("69da0c2a"),
+        parseHex("06e3e4c4"), parseHex("1095018e"), parseHex("0deea348"), parseHex("1f4c5513"),
+        parseHex("4f9a3a98"), parseHex("3179112b"), parseHex("524abb1f"), parseHex("21615ba2"),
+        parseHex("23ab4065"), parseHex("1202a1d1"), parseHex("21d25b83"), parseHex("6ed17c2f"),
     },
     .{ // Round 7 (final)
-        parseHex("021ff023"), parseHex("138a1240"), parseHex("0e311f53"), parseHex("257b1aaf"),
-        parseHex("07261fc2"), parseHex("0314803b"), parseHex("2116f6f8"), parseHex("20b26b1c"),
-        parseHex("05665b94"), parseHex("05f3f247"), parseHex("2e56bc50"), parseHex("2dd09124"),
-        parseHex("140ab0bc"), parseHex("08b4c979"), parseHex("1def1997"), parseHex("1b71e60d"),
-        parseHex("31e9b6e5"), parseHex("6b7e64ea"), parseHex("30d86629"), parseHex("34dafad9"),
-        parseHex("5e49bd32"), parseHex("4fe3cb3c"), parseHex("01a97a4c"), parseHex("14cd43db"),
+        parseHex("391e6b09"), parseHex("5e4ed894"), parseHex("6a2f58f2"), parseHex("5d980d70"),
+        parseHex("3fa48c5e"), parseHex("1f6366f7"), parseHex("63540f5f"), parseHex("6a8235ed"),
+        parseHex("14c12a78"), parseHex("6edde1c9"), parseHex("58ce1c22"), parseHex("718588bb"),
+        parseHex("334313ad"), parseHex("7478dbc7"), parseHex("647ad52f"), parseHex("39e82049"),
+        parseHex("6fee146a"), parseHex("082c2f24"), parseHex("1f093015"), parseHex("30173c18"),
+        parseHex("53f70c0d"), parseHex("6028ab0c"), parseHex("2f47a1ee"), parseHex("26a6780e"),
     },
 };
 
 // Internal round constants from plonky3 KoalaBear width-24 (23 rounds)
 const INTERNAL_RCS = [INTERNAL_ROUNDS]u32{
-    parseHex("353ef11f"), parseHex("180d911f"), parseHex("514bd047"), parseHex("6317e349"),
-    parseHex("001f2e7a"), parseHex("7dac1d74"), parseHex("37871e8e"), parseHex("7cb4d14e"),
-    parseHex("242d6f0f"), parseHex("0b07bd7d"), parseHex("386304f0"), parseHex("507b004e"),
-    parseHex("60e39ce0"), parseHex("0748e068"), parseHex("34869de1"), parseHex("08a53a5e"),
-    parseHex("3f246984"), parseHex("5806f3cd"), parseHex("13fd66f2"), parseHex("61c35b2a"),
-    parseHex("68cf3dcf"), parseHex("5c7a03aa"), parseHex("289efdfe"),
+    parseHex("3540bc83"), parseHex("1812b49f"), parseHex("5149c827"), parseHex("631dd925"),
+    parseHex("001f2dea"), parseHex("7dc05194"), parseHex("3789672e"), parseHex("7cabf72e"),
+    parseHex("242dbe2f"), parseHex("0b07a51d"), parseHex("38653650"), parseHex("50785c4e"),
+    parseHex("60e8a7e0"), parseHex("07464338"), parseHex("3482d6e1"), parseHex("08a69f1e"),
+    parseHex("3f2aff24"), parseHex("5814c30d"), parseHex("13fecab2"), parseHex("61cb291a"),
+    parseHex("68c8226f"), parseHex("5c757eea"), parseHex("289b4e1e"),
 };
 
 fn parseHex(s: []const u8) u32 {
