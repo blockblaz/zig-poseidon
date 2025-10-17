7c046844fa96eca310401412324080e1d5c81f38
Chetany Bhardwaj <chetanyiot@gmail.com>
2025-10-17 13:15:41 +0530

    fix: correct Montgomery form handling in compress

--- src/poseidon2/poseidon2.zig
+++ src/poseidon2/poseidon2.zig
@@ -38,10 +38,16 @@ pub fn Poseidon2(
             }
             permutation(&state);
             inline for (0..width) |i| {
-                F.add(&state[i], state[i], input[i]);
-                F.fromMontgomery(&state[i], state[i]);
+                var input_mont: F.MontFieldElem = undefined;
+                F.toMontgomery(&input_mont, input[i]);
+                F.add(&state[i], state[i], input_mont);
             }
-            return state[0..output_len];
+
+            var result: [output_len]F.FieldElem = undefined;
+            inline for (0..output_len) |i| {
+                result[i] = F.toNormal(state[i]);
+            }
+            return result;
         }
 
         pub fn permutation(state: *State) void {
